5d0182cd02c210971357a1de08c5125d2533eecd
Samuel Kostko <kostkosamuel43@gmail.com>
2026-09-01 23:15:56 -0400

    CAMBIOS EN PROMOCIONES

--- admin-promotions.js
+++ admin-promotions.js
@@ -81,6 +81,7 @@ const handleImageSelect = (file, target) => {
   targetImg.onload = () => {
     if (currentCropper) currentCropper.destroy();
     currentCropper = new Cropper(targetImg, {
+      aspectRatio: 4 / 3,
       viewMode: 1,
       autoCropArea: 1,
       responsive: true,
