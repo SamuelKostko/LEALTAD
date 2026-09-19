9d7defa52eb461038853264f0112eefe0faf242b
Samuel Kostko <kostkosamuel43@gmail.com>
2026-09-19 15:52:04 -0400

    FIXING the SAME

--- api/buy-points.js
+++ api/buy-points.js
@@ -86,6 +86,7 @@ export default async function handler(req, res) {
           ? {
               amount: parseFloat(amountClient.toFixed(2)),
               phone: clientPhone,
+              cedula: clientCedula,
               sede: defaultSede
             }
           : {
